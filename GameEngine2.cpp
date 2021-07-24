cf2988368e43b3aa94b989997c47d1ea8cd6471f
Stuart Mathews <stumathews@gmail.com>
2021-07-24 19:15:02 +0100

    wip

--- GameEngine2.cpp
+++ GameEngine2.cpp
@@ -1,20 +1,24 @@
 // GameEngine2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <D3DX10.h>
 #include <iostream>
+#include <windows.h>
 
-int main()
+auto main() -> int
 {
-    std::cout << "Hello World!\n";
+	try
+	{
+		
+		D3DXVECTOR3 vec1(1.0f, 2.0f, 3.0f);
+		D3DXVECTOR3 vec2(1.0f, 2.0f, 6.0f);
+		auto resVec = vec1 + vec2;
+		std::cout << "Resulting vector is:" << resVec.x << std::endl;        
+        return 0;
+		
+	}
+    catch(std::exception &e)
+    {
+		std::cout << "Exception occured: " << e.what() << std::endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
